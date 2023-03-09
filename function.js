1fd1f5b1ae01b8cb8197cf6fd789200fc687a9f1
Orelchalfon <103241272+Orelchalfon@users.noreply.github.com>
2023-03-09 11:37:28 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,9 +1,15 @@
 exports.MaxVal = (a, b, c) => Math.max(a, b, c);
 exports.PrintStrN_T = (str, num) => {
   for (let index = 0; index < num; index++) {
-     console.log(str);
+    console.log(str);
   }
 };
-exports.HowManyAfterP_Dig=(number)=>{
-    
+exports.HowManyAfterP_Dig = (num) => {
+  //parsInt
+  let counter = 0;
+  while (num - parseInt(num) > 0) {
+    counter++;
+    num *= 10;
+  }
+  return counter;
 };
